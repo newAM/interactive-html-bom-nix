d90046ce8903d2cc6db13b5bd3e8eebdaf426e16
Alex Martens <alex@thinglab.org>
2024-09-01 09:04:25 -0700

    Format with alejandra

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
-  description = "A very basic flake";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
-      packages.x86_64-linux.default = pkgs.callPackage ./package.nix { };
-      apps.x86_64-linux.default = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.default}/bin/generate_interactive_bom";
-      };
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.default = pkgs.callPackage ./package.nix {};
+  };
 }
--- package.nix
+++ package.nix
@@ -1,10 +1,9 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, makeWrapper
-, python3
+{
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+  python3,
 }:
-
 stdenv.mkDerivation rec {
   pname = "interactive-html-bom";
   version = "2.5.0";
@@ -16,7 +15,7 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-ZpytvUnQnjJ90mxMe4G3X8T0TUqz7DW9cxOLtJxn55w=";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [makeWrapper];
 
   prePatch = ''
     rm InteractiveHtmlBom/__init__.py
@@ -28,23 +27,22 @@ stdenv.mkDerivation rec {
     sed -i '/from ..dialog/d' $(find . -name '*.py')
   '';
 
-  installPhase =
-    let
-      python3WithKicad = python3.withPackages (p: with p; [
+  installPhase = let
+    python3WithKicad = python3.withPackages (p:
+      with p; [
         kicad
       ]);
-    in
-    ''
-      mkdir -p $out/opt
-      mkdir -p $out/bin
+  in ''
+    mkdir -p $out/opt
+    mkdir -p $out/bin
 
-      cp -r InteractiveHtmlBom $out/opt
+    cp -r InteractiveHtmlBom $out/opt
 
-      makeWrapper \
-        ${python3WithKicad}/bin/python \
-        $out/bin/generate_interactive_bom \
-        --set INTERACTIVE_HTML_BOM_NO_DISPLAY 1 \
-        --set INTERACTIVE_HTML_BOM_CLI_MODE 1 \
-        --add-flags $out/opt/InteractiveHtmlBom/generate_interactive_bom.py
-    '';
+    makeWrapper \
+      ${python3WithKicad}/bin/python \
+      $out/bin/generate_interactive_bom \
+      --set INTERACTIVE_HTML_BOM_NO_DISPLAY 1 \
+      --set INTERACTIVE_HTML_BOM_CLI_MODE 1 \
+      --add-flags $out/opt/InteractiveHtmlBom/generate_interactive_bom.py
+  '';
 }
